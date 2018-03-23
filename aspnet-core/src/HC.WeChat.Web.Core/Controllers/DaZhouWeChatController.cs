189dd1c0717adee0cba92a2af9e4b09b3a863335
Donald <tangdezou@qq.com>
2018-03-23 16:51:22 +0800

    多微信集成问题处理

--- aspnet-core/src/HC.WeChat.Web.Core/Controllers/DaZhouWeChatController.cs
+++ aspnet-core/src/HC.WeChat.Web.Core/Controllers/DaZhouWeChatController.cs
@@ -1,6 +1,8 @@
 ﻿using HC.WeChat.MessageHandler;
+using HC.WeChat.Models.WeChat;
 using HC.WeChat.WechatAppConfigs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,10 +12,21 @@ namespace HC.WeChat.Controllers
     [Route("[controller]/[action]")]
     public class DaZhouWeChatController : WeChatMessageHandlerControllerBase
     {
-        protected override string TenantConfigName => "DaZhou";
+        private WeChatTenantSetting _settings;
 
-        public DaZhouWeChatController(IMessageHandlerAppServer messageHandlerAppServer, IWechatAppConfigAppService wechatAppConfigAppService) : base(messageHandlerAppServer, wechatAppConfigAppService)
+        private int tenantId;
+
+        public DaZhouWeChatController(IMessageHandlerAppServer messageHandlerAppServer, 
+            IWechatAppConfigAppService wechatAppConfigAppService, 
+            IOptions<WeChatTenantSetting> settings) : base(messageHandlerAppServer, wechatAppConfigAppService)
         {
+            _settings = settings.Value;
+            tenantId = _settings.DaZhou;
+        }
+
+        protected override int GetTenantId()
+        {
+            return tenantId;
         }
     }
 }
