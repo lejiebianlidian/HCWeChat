5986e4fd65f767f373eef709b8e8005a063a703c
DonaldTdz <tangdezou@qq.com>
2018-04-11 23:00:38 +0800

    上传图片修改

--- aspnet-core/src/HC.WeChat.Web.Core/Models/WeChat/ActivityBanquetModel.cs
+++ aspnet-core/src/HC.WeChat.Web.Core/Models/WeChat/ActivityBanquetModel.cs
@@ -3,12 +3,15 @@ using Senparc.Weixin.MP.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace HC.WeChat.Models.WeChat
 {
     public class ActivityBanquetModel
     {
-        private string host { get; set; }
+        public ActivityBanquetModel()
+        {
+        }
         public JsSdkUiPackage JsSdkApiConfig { get; set; }
 
         public ActivityBanquetWeChatDto BanquetWeChat { get; set; }
@@ -19,7 +22,8 @@ namespace HC.WeChat.Models.WeChat
             {
                 if (BanquetWeChat != null && BanquetWeChat.PhotoUrl != null)
                 {
-
+                    var srcs = BanquetWeChat.PhotoUrl.Split(',');
+                    return srcs.ToList();
                 }
                 return new List<string>();
             }
