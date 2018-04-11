c3975e7fdc60fec57bdd98522fce685fab1e9508
Donald <tangdezou@qq.com>
2018-04-11 17:58:43 +0800

    上传宴席信息修改

--- aspnet-core/src/HC.WeChat.Web.Core/Models/WeChat/ActivityBanquetModel.cs
+++ aspnet-core/src/HC.WeChat.Web.Core/Models/WeChat/ActivityBanquetModel.cs
@@ -8,8 +8,21 @@ namespace HC.WeChat.Models.WeChat
 {
     public class ActivityBanquetModel
     {
+        private string host { get; set; }
         public JsSdkUiPackage JsSdkApiConfig { get; set; }
 
         public ActivityBanquetWeChatDto BanquetWeChat { get; set; }
+
+        public List<string> PhotoUrls
+        {
+            get
+            {
+                if (BanquetWeChat != null && BanquetWeChat.PhotoUrl != null)
+                {
+
+                }
+                return new List<string>();
+            }
+        }
     }
 }
