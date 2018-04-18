792cafb1592f1282d9e47896554b0a1a525d9708
shiguanWXX <1358110409@qq.com>
2018-04-18 16:01:10 +0800

    Excel信息增加活动申请单信息

--- aspnet-core/src/HC.WeChat.Application/ActivityForms/Dtos/PostInfoDto.cs
+++ aspnet-core/src/HC.WeChat.Application/ActivityForms/Dtos/PostInfoDto.cs
@@ -75,6 +75,60 @@ namespace HC.WeChat.ActivityForms.Dtos
         public bool TIsSend { get; set; }
         public string TIsSendName { get { return TIsSend == true ? "是" : "否"; } }
         public string IsSendName { get { return IsSend == true ? "是" : "否"; } }
+
+        //活动申请单信息
+        public Guid ActivityId { get; set; }
+        public Guid? RetailerId { get; set; }
+        public Guid? ActivityGoodsId { get; set; }
+        public string Reason { get; set; }
+        public FormStatusEnum Status { get; set; }
+
+        public string StatusName
+        {
+            get
+            {
+                return Status.ToString();
+            }
+        }
+
+        public string StatusType
+        {
+            get
+            {
+                switch (Status)
+                {
+                    case FormStatusEnum.提交申请:
+                        return "error";
+                    case FormStatusEnum.初审通过:
+                        return "processing";
+                    case FormStatusEnum.拒绝:
+                        return "default";
+                    case FormStatusEnum.资料回传已审核:
+                        return "success";
+                    case FormStatusEnum.营销中心已审核:
+                        return "success";
+                    case FormStatusEnum.取消:
+                        return "default";
+                    default: return "default";
+                }
+            }
+        }
+
+        public string ActivityName { get; set; }
+
+        public string RetailerName { get; set; }
+
+        public string ManagerName { get; set; }
+
+        public Guid? ManagerId { get; set; }
+
+        public string CreationUser { get; set; }
+
+        public Guid? CreationId { get; set; }
+
+        public List<ActivityFormLogDto> FormLogList { get; set; }
+
+        public int CurrentStep { get; set; }
     }
 
 }
